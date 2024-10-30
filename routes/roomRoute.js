c826c8c0ad1552bbcbc7bf40e1f0c44c69499bfe
RatRin <ratthakit.k@ku.th>
2024-10-30 15:00:50 +0700

    edit get to post in searchRoomByType

--- routes/roomRoute.js
+++ routes/roomRoute.js
@@ -9,7 +9,8 @@ router.get('/admin/AllRoom', roomC.searchAllRoom);
 router.get('/admin/PaginationRoom/:page', roomC.PaginationRoom);
 router.get('/admin/searchRoom/:id?', roomC.searchRoom);
 router.get('/admin/room/:id', roomC.getRoomById);
-router.get('/rooms', roomC.searchRoomByType);
+
+router.post('/rooms', roomC.searchRoomByType);
 
 
 
